8536a54d2a48996c12bebb511e6e36d3e36dccc2
tina2042 <tina2042@gmail.com>
2025-03-13 10:24:26 +0100

    Hotjar script added

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -16,3 +16,25 @@ describe('Swiper Gallery Test', function () {
     cy.get('.swiper-slide-active').should('contain', 'Paris');
   });
 });
+
+describe('Swiper Gallery Test', function () {
+  const slides = [
+    { title: 'Rome', country: 'Italy' },
+    { title: 'London', country: 'United Kingdom' },
+    { title: 'Paris', country: 'France' }
+  ];
+
+  it('Checks title and description of each slide', function () {
+    cy.visit('http://localhost:3000');
+
+    slides.forEach((slide, index) => {
+      if (index > 0) {
+        cy.get('.swiper-button-next').click();
+        cy.wait(2000);
+      }
+      cy.get('.swiper-slide-active').should('contain', slide.title);
+      cy.get('.swiper-slide-active').should('contain', slide.country);
+    });
+  });
+});
+
